e8136bfa52bc30150985f4337fa58fc592148dd0
Newton Munene <newtonmunene99@gmail.com>
2019-08-22 14:08:45 +0300

    Update page transition animation

--- lib/services/router.dart
+++ lib/services/router.dart
@@ -15,7 +15,7 @@ class Router {
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             var begin = Offset(0.0, 1.0);
             var end = Offset.zero;
-            var curve = Curves.easeIn;
+            var curve = Curves.linear;
 
             var tween =
                 Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
@@ -34,7 +34,7 @@ class Router {
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             var begin = Offset(0.0, 1.0);
             var end = Offset.zero;
-            var curve = Curves.easeIn;
+            var curve = Curves.linear;
 
             var tween =
                 Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
